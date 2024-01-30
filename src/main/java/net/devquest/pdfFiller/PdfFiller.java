b77031e82d90920f788994eeac17c8891670c278
DeQ <DeQ@github.dq78>
2024-01-30 22:04:50 +0100

    init

---
+++ src/main/java/net/devquest/pdfFiller/PdfFiller.java
@@ -0,0 +1,11 @@
+package net.devquest.pdfFiller;
+
+public class PdfFiller {
+	public static void main(String[] args) {
+		run();
+	}
+
+	protected static void run() {
+		System.out.println("Hello World!");
+	}
+}
---
+++ src/test/java/net/devquest/pdfFiller/PdfFillerTest.java
@@ -0,0 +1,14 @@
+package net.devquest.pdfFiller;
+
+import static org.junit.Assume.assumeTrue;
+
+import org.junit.jupiter.api.Test;
+
+class PdfFillerTest {
+
+	@Test
+	void run() {
+		assumeTrue(true);
+	}
+
+}
